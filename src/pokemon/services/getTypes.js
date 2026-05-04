6328495f5189d60cca054a34090ed3825753cefe
sask <angbmz364@gmail.com>
2026-05-04 14:13:06 -0500

    refactor: getTypes now get all pokemon data

--- src/pokemon/services/getTypes.js
+++ src/pokemon/services/getTypes.js
@@ -1,4 +1,4 @@
-async function getTypes(url) {
+async function getPokeData(url) {
   const res = await  fetch(url);
 
   if (!res.ok) {
@@ -6,7 +6,7 @@ async function getTypes(url) {
   }
 
   const data = await res.json();
-  return data.types.map(t => t.type.name);
+  return data;
 }
 
-export {getTypes};
+export {getPokeData};
